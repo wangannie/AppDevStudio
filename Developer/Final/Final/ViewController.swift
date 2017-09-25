373293c024184a58704cd2691bb54e8a3bdc83c9
wangannie <anniewang@Annies-MacBook-Air.local>
2017-09-25 08:42:38 -0400

    2nd 1st commit

---
+++ Developer/Final/Final/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Final
+//
+//  Created by Annie Wang on 9/25/17.
+//  Copyright © 2017 Annie Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
