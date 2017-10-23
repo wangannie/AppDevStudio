71c6c791a8cec49fee194b3b81373baf5afcdd70
higginsk <higginsk@hs-c106-53930m.local>
2017-10-23 08:58:39 -0400

    Kevin did gud

---
+++ Developer/Final/Final/MapViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MapViewController.swift
+//  Final
+//
+//  Created by higginsk on 10/13/17.
+//  Copyright © 2017 Annie Wang. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var BtnMain: UIButton!
+    
+    @IBAction func pressed(_ sender: UIButton) {
+        if sender.titleLabel?.text == "Main Menu"{
+            let mainstoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = mainstoryboard.instantiateViewController(withIdentifier: "MainViewController")
+            self.present(vc, animated: true, completion: nil)
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.init(red: 42.0/255, green: 168.0/255, blue: 28.0/255, alpha: 1.0)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
--- Developer/Final/Final/ViewController.swift
+++ Developer/Final/Final/ViewController.swift
@@ -10,14 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBAction func pressed(_ sender: Any) {
-        print("button pressed")
+    @IBOutlet weak var BtnMap: UIButton!
+    @IBOutlet weak var BtnNot: UIButton!
+    
+    @IBAction func pressed(_ sender: UIButton) {
+        if sender.titleLabel?.text == "Map"{
+            let mapstoryboard = UIStoryboard(name: "Map", bundle: nil)
+            let vc = mapstoryboard.instantiateViewController(withIdentifier: "MapViewController")
+            self.present(vc, animated: true, completion: nil)
+            
+        }
+        else if sender.titleLabel?.text == "Notify Person and Wait"{
+            let notifystoryboard = UIStoryboard(name: "Notify", bundle: nil)
+            let vc = notifystoryboard.instantiateViewController(withIdentifier: "NotifyViewController")
+            self.present(vc, animated: true, completion: nil)
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.init(red: 42.0/255, green: 168.0/255, blue: 28.0/255, alpha: 1.0)
+        //self.view.backgroundColor = UIColor(patternImage: UIImage("Our picture")!)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
